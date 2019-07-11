35b88819fe4cfb23bc6f130960b3822b8a4d27f7
503231259 <l503231259@163.com>
2019-07-12 07:34:35 +0800

    首页基本布局

--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -1,6 +1,29 @@
 <template>
   <div>
-    <p>home</p>
+    <van-nav-bar
+      title="首页"
+    />
+    <van-tabs v-model="activeChannelIndex">
+      <van-tab title="标签1">
+        <van-pull-refresh v-model="isLoading" @refresh="onRefresh">
+          <van-list
+           v-model="loading"
+           :finished="finished"
+           finished-text="没有更多了"
+           @load="onLoad"
+          >
+            <van-cell
+              v-for="item in list"
+              :key="item"
+              :title="item"
+            />
+          </van-list>
+        </van-pull-refresh>
+      </van-tab>
+      <van-tab title="标签 2">内容 2</van-tab>
+      <van-tab title="标签 3">内容 3</van-tab>
+      <van-tab title="标签 4">内容 4</van-tab>
+    </van-tabs>
   </div>
 </template>
 <script>
@@ -8,6 +31,38 @@ export default {
   name: 'home',
   data () {
     return {
+      activeChannelIndex: 0,
+      // 上拉
+      list: [],
+      loading: false,
+      finished: false,
+      // 下拉
+      isLoading: false
+    }
+  },
+  methods: {
+    // 上拉下拉
+    onLoad () {
+      // 异步更新数据
+      setTimeout(() => {
+        for (let i = 0; i < 10; i++) {
+          this.list.push(this.list.length + 1)
+        }
+        // 加载状态结束
+        this.loading = false
+
+        // 数据全部加载完成
+        if (this.list.length >= 40) {
+          this.finished = true
+        }
+      }, 500)
+    },
+    onRefresh () {
+      setTimeout(() => {
+        this.$toast('刷新成功')
+        this.isLoading = false
+        this.count++
+      }, 500)
     }
   }
 }
